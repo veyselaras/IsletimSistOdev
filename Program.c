c1bc5a61701d0045ed8f40aa5598c3383eec160d
NulliussInVerba <veysel.aras@ogr.sakarya.edu.tr>
2024-12-20 14:54:54 +0300

    kod parçacıkları eklendi

--- Program.c
+++ Program.c
@@ -1,3 +1,49 @@
+#include "program.h"
+
+// Arka plan süreçleri bittiğinde yazdırmak için handler
+void sigchld_handler(int signo) {
+    int durum;
+    pid_t pid;
+    // Non-blocking wait
+    while ((pid = waitpid(-1, &durum, WNOHANG)) > 0) {
+        // Arka plan listeden çıkar ve sonucu göster
+        printf("[%d] retval: %d\n", pid, WEXITSTATUS(durum));
+        fflush(stdout);
+        arkaplanProcess_t** onceki = &arkaplanListesi;
+        arkaplanProcess_t* suanki = arkaplanListesi;
+        while (suanki) {
+            if (suanki->pid == pid) {
+                *onceki = suanki->sonraki;
+                free(suanki);
+                break;
+            }
+            onceki = &suanki->sonraki;
+            suanki = suanki->sonraki;
+        }
+    }
+}
+//ismail
+// Arka plan sürecini listeye ekleme
+void arkaPlanProcessEkle(pid_t pid) {
+    arkaplanProcess_t* yeniProcess = malloc(sizeof(arkaplanProcess_t));
+    yeniProcess->pid = pid;
+    yeniProcess->sonraki = arkaplanListesi;
+    arkaplanListesi = yeniProcess;
+}
+
+// Tüm arka plan süreçlerinin bitmesini beklemek
+void arkplanBekle() {
+    arkaplanProcess_t* suanki = arkaplanListesi;
+    while (suanki) {
+        int durum;
+        waitpid(suanki->pid, &durum, 0);
+        printf("[%d] retval: %d\n", suanki->pid, WEXITSTATUS(durum));
+        arkaplanProcess_t* temp = suanki;
+        suanki = suanki->sonraki;
+        free(temp);
+    }
+}
+
 // Arka plan sürecini listeye ekleme
 void arkaPlanProcessEkle(pid_t pid) {
     arkaplanProcess_t* yeniProcess = malloc(sizeof(arkaplanProcess_t));
